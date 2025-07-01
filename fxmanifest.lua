5beff9fe3b74e36b78410e40597bd92ea7cc3a18
Code Wizards <94300419+CodeWizardsDev@users.noreply.github.com>
2025-07-02 01:24:51 +0330

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,19 +7,17 @@ description 'Library for my FIVEM scripts:)'
 version '1.0.0'
 
 shared_scripts {
-	'@ox_lib/init.lua',
+    '@ox_lib/init.lua',
     'config.lua'
 }
 
 client_scripts {
     'client/functions.lua',
-    'client/events.lua'
 }
 
 server_scripts {
     'server/functions.lua',
     'server/events.lua',
-    'server/server.lua'
 }
 
 ox_libs {
